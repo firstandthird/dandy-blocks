a7895009cf9eee2bff26eb73308da177eaa44731
ApplianceJohn <thednbgremlin@gmail.com>
2023-01-30 14:33:18 -0500

    added color selects to hero block

--- blocks/hero/hero.php
+++ blocks/hero/hero.php
@@ -22,6 +22,9 @@ $link_2      = get_field( 'button_2_link' );
 $bg_image    = wp_get_attachment_image_url( get_field( 'bg_image' ), 'full' );
 $bg_x        = get_field( 'background_position' )['x'];
 $bg_y        = get_field( 'background_position' )['y'];
+$primary     = get_field( 'primary_color' );
+$secondary   = get_field( 'secondary_color' );
+$tertiary    = get_field( 'button_text_color' );
 
 // Support custom "anchor" values.
 $anchor = '';
@@ -49,30 +52,30 @@ $has_any_button = ( $button_1 && $link_1 ) || ( $button_2 && $link_2 );
   <div class="flex w-full items-center justify-center min-h-[50vh]" style="background-image: url(<?php echo esc_url( $bg_image ); ?>); background-position: <?php echo esc_attr( $bg_x . '% ' . $bg_y . '%' ); ?>;">
     <div class="w-full max-w-4xl p-5">
       <?php if ( $superheader ) : ?>
-        <p class="text-sm leading-tight"><?php echo esc_html( $superheader ); ?></p>
+        <p class="text-sm leading-tight" style="color:<?= esc_attr($secondary) ?>;"><?php echo esc_html( $superheader ); ?></p>
       <?php endif; ?>
       <?php if ( $header ) : ?>
-        <h1 class="text-5xl leading-snug"><?php echo esc_html( $header ); ?></h1>
+        <h1 class="text-5xl leading-snug" style="color:<?= esc_attr($primary) ?>;"><?php echo esc_html( $header ); ?></h1>
       <?php endif; ?>
       <?php if ( $subheader ) : ?>
-        <p class="text-sm leading-tight">
+        <p class="text-sm leading-tight" style="color:<?= esc_attr($secondary) ?>;">
           <?php echo esc_html( $subheader ); ?>
         </p>
       <?php endif; ?>
       <?php if ( $content ) : ?>
-        <p class="text-base leading-normal mt-4">
+        <p class="text-base leading-normal mt-4" style="color:<?= esc_attr($secondary) ?>;">
           <?php echo esc_html( $content ); ?>
         </p>
       <?php endif; ?>
       <?php if ( $has_any_button ) : ?>
         <div class="mt-4 flex flex-row gap-x-5 gap-y-2">
           <?php if ( $button_1 && $link_1 ) : ?>
-          <a class="bg-black text-white px-5 py-3 leading-none" href="<?= esc_url( $link_1 ); ?>">
+          <a class="px-5 py-3 leading-none" href="<?= esc_url( $link_1 ); ?>" style="color:<?= esc_attr($tertiary) ?>;background-color:<?= esc_attr($primary) ?>;">
             <?php echo esc_html( $button_1 ); ?>
           </a>
           <?php endif; ?>
           <?php if ( $button_2 && $link_2 ) : ?>
-          <a class="bg-black text-white px-5 py-3 leading-none" href="<?= esc_url( $link_2 ); ?>">
+          <a class="px-5 py-3 leading-none" href="<?= esc_url( $link_2 ); ?>" style="color:<?= esc_attr($tertiary) ?>;background-color:<?= esc_attr($primary) ?>;">
             <?php echo esc_html( $button_2 ); ?>
           </a>
           <?php endif; ?>
